2b087492e0b30f885dd0d514167444a7686f3fe8
Holder-hash <X-Lamamen-X@yandex.ru>
2023-08-19 00:20:37 +0300

    language popup update

--- js/language-popup.js
+++ js/language-popup.js
@@ -4,13 +4,13 @@ const infoBtn = document.querySelector('#infoBtn')
 
 languageBtn.onclick = () => {
     languagePopupFade.style.display = 'block';
-    languagePopupFade.style.opacity = '1';
+    setTimeout(() => languagePopupFade.style.opacity = '1', .1);
 }
 
 document.addEventListener('click', (e) => {
     const hidden = e.composedPath().includes(languagePopupFade);
     if (!hidden && e.target != languageBtn) {
-        languagePopupFade.style.display = 'none';
+        setTimeout(() => languagePopupFade.style.display = 'none', 500);
         languagePopupFade.style.opacity = '0';
     }
 })
