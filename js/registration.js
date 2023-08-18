1b3fb51b135a05147d947c95dc9429cef4b41baf
Holder-hash <X-Lamamen-X@yandex.ru>
2023-08-19 02:15:33 +0300

    reg form input update (dynamic legend)

--- js/registration.js
+++ js/registration.js
@@ -2,7 +2,9 @@ $(document).ready(function($) {
     $('#registrationNameInput').focus(() => 
         $('.registration-form-name-box legend').css('transform', 'translate(0,0)')
     );
-    $('#registrationNameInput').blur(() => 
-        $('.registration-form-name-box legend').css('transform', 'translate(0,20px)')
-    );
+    $('#registrationNameInput').blur(function() {
+        if (!$('#registrationNameInput').val()) {
+            $('.registration-form-name-box legend').css('transform', 'translate(0,20px)')
+        }    
+    })
 });	
