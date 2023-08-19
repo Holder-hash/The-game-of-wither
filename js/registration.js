bb9a3deac49a3b72c84f646ec918e42cc2655b2b
Holder-hash <X-Lamamen-X@yandex.ru>
2023-08-19 04:58:17 +0300

    signature canvas added and working

--- js/registration.js
+++ js/registration.js
@@ -10,3 +10,31 @@ $(document).ready(function($) {
 });	
 
 //signature canvas
+
+var canvas = document.getElementById('signatureCanvas');
+var ctx = canvas.getContext('2d');
+
+canvas.width  = 200;
+canvas.height = 50;
+
+var mouseDown = false;
+
+canvas.addEventListener('mousedown', () => {
+    mouseDown = true;
+});
+
+canvas.addEventListener('mouseup', () => {
+    mouseDown = false;
+    ctx.beginPath();
+});
+
+ctx.lineWidth = 3;
+canvas.addEventListener('mousemove', (e) => {
+    if (mouseDown) {
+        ctx.lineTo(e.offsetX, e.offsetY);
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(e.offsetX, e.offsetY);
+    }
+});
