4037b7b372b3a7d3e7e1fa5cbe3487bd5cf7d1c1
Holder-hash <X-Lamamen-X@yandex.ru>
2023-08-19 05:32:06 +0300

    validation added

--- js/registration.js
+++ js/registration.js
@@ -1,3 +1,6 @@
+var haveName = false;
+var haveSignature = false;
+
 $(document).ready(function($) {
     $('#registrationNameInput').focus(() => 
         $('.registration-form-name-box legend').css('transform', 'translate(0,0)')
@@ -5,36 +8,54 @@ $(document).ready(function($) {
     $('#registrationNameInput').blur(function() {
         if (!$('#registrationNameInput').val()) {
             $('.registration-form-name-box legend').css('transform', 'translate(0,20px)')
-        }    
+        }
+        else {
+            haveName = true;
+        }
+    })
+
+    //signature canvas
+    
+    var canvas = document.getElementById('signatureCanvas');
+    var ctx = canvas.getContext('2d');
+    var mouseDown = false;
+    
+    canvas.width  = 200;
+    canvas.height = 50;
+    
+    
+    canvas.addEventListener('mousedown', () => {
+        mouseDown = true;
+    });
+    
+    canvas.addEventListener('mouseup', () => {
+        mouseDown = false;
+        ctx.beginPath();
+    });
+    
+    ctx.lineWidth = 2;
+    canvas.addEventListener('mousemove', (e) => {
+        if (mouseDown) {
+            haveSignature = true;
+            ctx.lineTo(e.offsetX, e.offsetY);
+            ctx.stroke();
+    
+            ctx.beginPath();
+            ctx.moveTo(e.offsetX, e.offsetY);
+        }
+    });
+    
+    //startBtn
+    
+    $('#startBtn').click(function() {
+        if (haveName == true && haveSignature == true) {
+            $('.registration-window').fadeOut()
+        }
+        if (haveName == false) {
+            $('.registration-form-name-box').toggleClass('jerking-animation-class')
+        }
+        if (haveSignature == false) {
+            $('.registration-form-footer-signatuer-box').toggleClass('jerking-animation-class')
+        }
     })
 });	
-
-//signature canvas
-
-var canvas = document.getElementById('signatureCanvas');
-var ctx = canvas.getContext('2d');
-
-canvas.width  = 200;
-canvas.height = 50;
-
-var mouseDown = false;
-
-canvas.addEventListener('mousedown', () => {
-    mouseDown = true;
-});
-
-canvas.addEventListener('mouseup', () => {
-    mouseDown = false;
-    ctx.beginPath();
-});
-
-ctx.lineWidth = 3;
-canvas.addEventListener('mousemove', (e) => {
-    if (mouseDown) {
-        ctx.lineTo(e.offsetX, e.offsetY);
-        ctx.stroke();
-
-        ctx.beginPath();
-        ctx.moveTo(e.offsetX, e.offsetY);
-    }
-});
