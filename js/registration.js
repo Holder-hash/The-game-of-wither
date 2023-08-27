84ee83d641edb305ffce231b4902d53be44855bf
Holder-hash <X-Lamamen-X@yandex.ru>
2023-08-28 01:50:22 +0300

    person level up witch items structure created

--- js/registration.js
+++ js/registration.js
@@ -68,21 +68,23 @@ $(document).ready(function($) {
 
 // game
 
+
+
 const gameArea = document.querySelector('.game-area')
 var personCardName = document.querySelector('#personCardName')
 var personLevelText = document.querySelector('#personLevelText')
 const enemyContainArea = document.querySelector('.enemy-contain-area')
 
+var personLevelUp = 0;
+var personSave = false;
 
-const person = {
+var person = {
     'name': 'none',
     'item': 'defaultItem1',
     'class': 'default',
     'level': 3
 }
 
-var personLevelUp;
-var personSave;
 
 function game() {
     gameArea.style.display = 'flex';
@@ -187,7 +189,7 @@ function game() {
                 if (value < person.level && enemyMan.destroyed == false) {
                     enemyMan.destroyed = true;
                     enemyPic.src = 'https://media.istockphoto.com/id/901964114/ru/%D1%84%D0%BE%D1%82%D0%BE/%D0%BD%D0%B0%D0%B4%D0%B3%D1%80%D0%BE%D0%B1%D0%B8%D0%B5-rip.jpg?s=612x612&w=0&k=20&c=a1CxXKdGhUpl4s-B0FA_T6_2_gQuZmxb1NaN-r60Ia4=';
-                    person.level += Number(value) / person.level;
+                    person.level += (Number(value) / person.level) + personLevelUp;
                     personLevelText.innerHTML = person.level.toFixed(2);
                     enemy.style = `cursor: not-allowed;`
 
@@ -605,6 +607,7 @@ vampirItemImg4.onclick = () => {
 
 if (person.item == 'defaultItem1') {
     personCardItemImg.src = defaultItemImg1.src
+    personLevelUp = 0;
 }
 
 //open a class img
