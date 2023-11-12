87cb52689b319ceef5b53b13090d20d1e47f8665
Holder-hash <X-Lamamen-X@yandex.ru>
2023-11-12 19:48:59 +0300

    carusel avatar changer done

--- js/carousel.js
+++ js/carousel.js
@@ -24,33 +24,48 @@ const classImg = document.querySelectorAll('.class-item');
 const gamePersonAvatar = document.getElementById('gamePersonAvatar');
 
 startBtn.onclick = () => {
-    if (carouselIndex == 0) gamePersonAvatar.src = 'media/persons/default/default1.jpeg'
-    else if (carouselIndex == -240) gamePersonAvatar.src = 'media/persons/default/default2.jpeg'
-    else if (carouselIndex == -480) gamePersonAvatar.src = 'media/persons/default/default3.jpeg'
-    else if (carouselIndex == -720) gamePersonAvatar.src = 'media/persons/default/default4.jpeg'
+    if (carouselIndex == 0 && localStorage.getItem('heroy') == 'default') gamePersonAvatar.src = 'media/persons/default/default1.jpeg'
+    else if (carouselIndex == -240 && localStorage.getItem('heroy') == 'default') gamePersonAvatar.src = 'media/persons/default/default2.jpeg'
+    else if (carouselIndex == -480 && localStorage.getItem('heroy') == 'default') gamePersonAvatar.src = 'media/persons/default/default3.jpeg'
+    else if (carouselIndex == -720 && localStorage.getItem('heroy') == 'default') gamePersonAvatar.src = 'media/persons/default/default4.jpeg'
+
+    if (carouselIndex == 0 && localStorage.getItem('heroy') == 'mainer') gamePersonAvatar.src = 'media/persons/mainer/mainer1.jpeg'
+    else if (carouselIndex == -240 && localStorage.getItem('heroy') == 'mainer') gamePersonAvatar.src = 'media/persons/mainer/mainer2.jpeg'
+    else if (carouselIndex == -480 && localStorage.getItem('heroy') == 'mainer') gamePersonAvatar.src = 'media/persons/mainer/mainer3.jpeg'
+    else if (carouselIndex == -720 && localStorage.getItem('heroy') == 'mainer') gamePersonAvatar.src = 'media/persons/mainer/mainer4.jpeg'
+
+    if (carouselIndex == 0 && localStorage.getItem('heroy') == 'witch') gamePersonAvatar.src = 'media/persons/witch/witch1.jpeg'
+    else if (carouselIndex == -240 && localStorage.getItem('heroy') == 'witch') gamePersonAvatar.src = 'media/persons/witch/witch2.jpeg'
+    else if (carouselIndex == -480 && localStorage.getItem('heroy') == 'witch') gamePersonAvatar.src = 'media/persons/witch/witch3.jpeg'
+    else if (carouselIndex == -720 && localStorage.getItem('heroy') == 'witch') gamePersonAvatar.src = 'media/persons/witch/witch4.jpeg'
+
+    if (carouselIndex == 0 && localStorage.getItem('heroy') == 'vampir') gamePersonAvatar.src = 'media/persons/vampir/vampir1.jpeg'
+    else if (carouselIndex == -240 && localStorage.getItem('heroy') == 'vampir') gamePersonAvatar.src = 'media/persons/vampir/vampir2.jpeg'
+    else if (carouselIndex == -480 && localStorage.getItem('heroy') == 'vampir') gamePersonAvatar.src = 'media/persons/vampir/vampir3.jpeg'
+    else if (carouselIndex == -720 && localStorage.getItem('heroy') == 'vampir') gamePersonAvatar.src = 'media/persons/vampir/vampir4.jpeg'
 }
 
-classImg.forEach(classImgItem => {
-    classImgItem.onclick = () => {
-        switch (localStorage.getItem('heroy')) {
-            case 'default':
+// classImg.forEach(classImgItem => {
+//     classImgItem.onclick = () => {
+//         switch (localStorage.getItem('heroy')) {
+//             case 'default':
 
-                break;
+//                 break;
         
-            case 'mainer':
-                console.log('mainer');
-                break;
+//             case 'mainer':
+//                 console.log('mainer');
+//                 break;
 
-            case 'witch':
-                console.log('witch');
-                break;
+//             case 'witch':
+//                 console.log('witch');
+//                 break;
 
-            case 'vampir':
-                console.log('vampir');
-                break;
+//             case 'vampir':
+//                 console.log('vampir');
+//                 break;
         
-            default:
-                break;
-        }
-    }
-})
+//             default:
+//                 break;
+//         }
+//     }
+// })
