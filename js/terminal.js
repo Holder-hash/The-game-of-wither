b075ad869c7e579ab713e5e4154de49bfa843dbd
Holder-hash <X-Lamamen-X@yandex.ru>
2023-11-15 03:05:56 +0300

    terminal open/close mechanic change & start do com

--- js/terminal.js
+++ js/terminal.js
@@ -4,15 +4,25 @@ const terminalTextarea = document.getElementById('terminalTextarea');
 const terminalWindow = document.querySelector('.terminal-window');
 
 terminalOpenBtn.onclick = () => {
-    terminalWindow.style.bottom = '115px';
+    terminalWindow.classList.toggle('terminal-open');
 }
 terminalCloseBtn.onclick = () => {
-    terminalWindow.style.bottom = '-490px';
+    terminalWindow.classList.toggle('terminal-open');
 }
 
 terminalTextarea.onkeydown= (e) => {
     if (e.keyCode === 13) {
-        e.preventDefault()
-    }
+        const commandValue = terminalTextarea.value;
+        e.preventDefault();
 
+        if (commandValue.includes('set prestige')) {
+            
+        }
+
+    }
+}
+
+function getLastValue(str) {
+    let words = str.split(' ');
+    return words.slice(-1);
 }
