ac590dc8c87b064ea5b6ff86e0f77f669ee9d82c
Holder-hash <X-Lamamen-X@yandex.ru>
2023-11-15 03:18:37 +0300

    terminal set prestige

--- js/terminal.js
+++ js/terminal.js
@@ -2,6 +2,8 @@ const terminalOpenBtn = document.getElementById('terminalOpenBtn');
 const terminalCloseBtn = document.getElementById('terminalCloseBtn');
 const terminalTextarea = document.getElementById('terminalTextarea');
 const terminalWindow = document.querySelector('.terminal-window');
+const terminalStatus = document.querySelector('.terminal__status');
+const terminalStatusSpan = document.querySelector('.terminal__status span');
 
 terminalOpenBtn.onclick = () => {
     terminalWindow.classList.toggle('terminal-open');
@@ -15,8 +17,19 @@ terminalTextarea.onkeydown= (e) => {
         const commandValue = terminalTextarea.value;
         e.preventDefault();
 
-        if (commandValue.includes('set prestige')) {
-            
+        if (commandValue.includes('set prestige') && Number(getLastValue(commandValue))) {
+            localStorage.prestige = getLastValue(commandValue);
+            terminalStatus.style.color = 'green';
+            terminalStatusSpan.textContent = 'success!'
+            terminalStatus.style.transition = 'color 0s';
+            setTimeout(() => {
+                terminalStatus.style.color = 'rgb(194, 194, 194)';
+                terminalStatus.style.transition = 'color .3s';
+            }, 3000)
+        }
+        else {
+            terminalStatus.style.color = 'red';
+            terminalStatusSpan.textContent = 'error!'
         }
 
     }
