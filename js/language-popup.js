190c5d39f94523feff0cb8b2d5d301df88ab22b9
Holder-hash <X-Lamamen-X@yandex.ru>
2023-11-16 20:16:48 +0300

    reg window translate

--- js/language-popup.js
+++ js/language-popup.js
@@ -20,29 +20,70 @@ const rusBox = document.querySelector('.russian-language-box');
 
 if (localStorage.getItem('language') == null) {
     localStorage.setItem('language', 'eng');
+    engBox.style.color = 'red';
 }
 
 engBox.onclick = () => {
     localStorage.language = 'eng';
     engBox.style.color = 'red';
     rusBox.style.color = 'rgb(194, 194, 194)';
+    ENGtranslate();
 }
 rusBox.onclick = () => {
     localStorage.language = 'rus';
     engBox.style.color = 'rgb(194, 194, 194)';
     rusBox.style.color = 'red';
+    RUStranslate();
 }
 
 if (localStorage.getItem('language') == 'eng') {
     engBox.style.color = 'red';
     rusBox.style.color = 'rgb(194, 194, 194)';
-
-
+    ENGtranslate();
 }
-
 if (localStorage.getItem('language') == 'rus') {
     engBox.style.color = 'rgb(194, 194, 194)';
     rusBox.style.color = 'red';
+    RUStranslate();
+}
 
-    
+function ENGtranslate() {
+    document.querySelector('.registration-form-title p').textContent = 'Registration of warriors';
+    document.querySelector('.registration-form-name-box legend').textContent = 'Name';
+    document.querySelector('.name-requirements-box span').title = 'Your name must be 2 or more characters long, contain no special characters, or be empty.';
+    document.querySelector('.registration-form-class-box legend').textContent = 'Class';
+    document.querySelector('.registration-form-starting-item-box legend').textContent = 'Item';
+    document.querySelector('.registration-form-footer-signatuer-box p').textContent = 'Signatuer';
+    document.querySelector('.registration-form-footer-start-btn p').textContent = 'To battle!';
+}
+
+function RUStranslate() {
+    document.querySelector('.registration-form-title p').textContent = 'Регистрация воинов';
+    document.querySelector('.registration-form-name-box legend').textContent = 'Имя';
+    document.querySelector('.name-requirements-box span').title = 'Ваше имя должно состоять из 2 или более символов, не содержать специальных символов или быть пустым.';
+    document.querySelector('.registration-form-class-box legend').textContent = 'Класс';
+    document.querySelector('.registration-form-starting-item-box legend').textContent = 'Предмет';
+    document.querySelector('.registration-form-footer-signatuer-box p').textContent = 'Подпись';
+    document.querySelector('.registration-form-footer-start-btn p').textContent = 'В бой!';
+    document.querySelector('').textContent = '';
+    document.querySelector('').textContent = '';
+    document.querySelector('').textContent = '';
+    document.querySelector('').textContent = '';
+    document.querySelector('').textContent = '';
+    document.querySelector('').textContent = '';
+    document.querySelector('').textContent = '';
+    document.querySelector('').textContent = '';
+    document.querySelector('').textContent = '';
+    document.querySelector('').textContent = '';
+    document.querySelector('').textContent = '';
+    document.querySelector('').textContent = '';
+    document.querySelector('').textContent = '';
+    document.querySelector('').textContent = '';
+    document.querySelector('').textContent = '';
+    document.querySelector('').textContent = '';
+    document.querySelector('').textContent = '';
+    document.querySelector('').textContent = '';
+    document.querySelector('').textContent = '';
+    document.querySelector('').textContent = '';
+    document.querySelector('').textContent = '';
 }
